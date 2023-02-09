8bdb44f03c5697ce37420a688f692f22f0c09cda
arad4228 <arad4228@gmail.com>
2023-02-09 12:55:47 +0900

    [KyoungJae]Add Cpp code

---
+++ CPPGIT/arad4228/Src/Arad4228/GlobalRingBuffer.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "GlobalRingBuffer.h"
+
+
+CGlobalRingBuffer::CGlobalRingBuffer(void): m_Buffer()
+{
+	m_Buffer.Create(9292);
+}
+CGlobalRingBuffer::~CGlobalRingBuffer(void)
+{
+	m_Buffer.Destroy();
+}
+void* CGlobalRingBuffer::Alloc(size_t tSize)
+{
+	return m_Buffer.Alloc(tSize);
+}
--- CPPGIT/arad4228/Src/Arad4228/main.cpp
+++ CPPGIT/arad4228/Src/Arad4228/main.cpp
@@ -1,17 +1,22 @@
 #include "pch.h"
+#include "GlobalRingBuffer.h"
 
 int main(void)
 {
-	HANDLE hFile = CreateFile(TEXT("C:\\Users\\arad4\\Desktop\\CPPGIT\\report.txt"), GENERIC_READ_, OPEN_EXISTING_, 0);
-	if (NULL == hFile)
-			return 1;
-	char szBuffer[10000 + 1];
-	DWORD dwReadSize = 0;
-	ReadFile(hFile, szBuffer, 10000, &dwReadSize);
-	
-	szBuffer[dwReadSize] = 0;
-	printf("%s\n", szBuffer);
-
+	HANDLE hFile = CreateFile(TEXT("C:\\Users\\arad4\\Desktop\\CPPGIT\\report.json"), GENERIC_READ_, OPEN_EXISTING_, 0);
+	std::list<char*> listTemp;
+	while (true)
+	{
+		const DWORD dwBufferSize = 100;
+		char* pBuffer = (char*)RingBuffer()->Alloc(dwBufferSize + 1);
+		DWORD dwReadSize = 0;
+		if (!ReadFile(hFile, pBuffer, dwBufferSize, &dwReadSize) || 0 == dwReadSize)
+			break;
+		pBuffer[dwReadSize] = 0;
+		listTemp.push_back(pBuffer);
+	}
 	CloseFile(hFile);
+	printf("%s", listTemp.front());
 	return 0;
+
 }
